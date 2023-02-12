aee57c1061cb21f25a2f6f275a155a0ff81ca252
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-02-12 17:57:48 +0600

    New Work

--- js/deposit.js
+++ js/deposit.js
@@ -1,16 +1,38 @@
 // step-1: add click event handler with the deposit button
 document.getElementById('btn-deposit').addEventListener('click', function () {
-    console.log('Dek Amio Pari ha ha ha ha ha');
 
     // step-2: get the amount inside the deposit input field
     // input field theke text pete hoile .value use korte hoy
+
     const depositInput = document.getElementById('user-deposit');
-    const deposit = depositInput.value;
-    console.log(deposit);
+    
+    // const deposit = depositInput.value;
+
+    const newDeposit = depositInput.value;
+
+    //Input k Number e Convert Korte Hobe
+    const newDepositAmount = parseFloat(newDeposit);
 
     //step-3: get the total Deposit amount
     //Non-Input(Textarea, element) value gula paite .innerText use korte hoy
+
     const depositTotalElement = document.getElementById('deposit-amount');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = deposit;
+    const oldDepositTotal = depositTotalElement.innerText;
+    const oldDepositAmount = parseFloat(oldDepositTotal);
+
+    //step 4:Number add kore Total deposit Ber korte hobe
+    const currentDepositeTotal = oldDepositAmount + newDepositAmount;
+    depositTotalElement.innerText = currentDepositeTotal;
+
+    //step-5: Get Current Balance
+    const mainBalance = document.getElementById('account-amount');
+    const totalMainAmount = mainBalance.innerText;
+    const totalBalance = parseFloat(totalMainAmount)
+
+    //Step-6: Calculate
+    const newMainBalance = totalBalance + newDepositAmount;
+    mainBalance.innerText = newMainBalance;
+
+    //step 7: Clear The Deposit inputField
+    depositInput.value ='';
 })
